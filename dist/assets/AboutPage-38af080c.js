ee94e4193d8838a5c7a8126071e43b12e2ad3642
Valiyev Javlon <valiyevjavlon001@gmail.com>
2023-08-17 19:20:08 +0500

    cool

---
+++ dist/assets/AboutPage-38af080c.js
@@ -0,0 +1,1 @@
+import{d as e,_ as t,o,c as n}from"./index-9b59ce11.js";const a=e({data(){return{}}});function r(s,c,p,_,u,d){return o(),n("h1",null,"About Us")}const l=t(a,[["render",r]]);export{l as default};
